11dd611df9416621e6513ef1e12facd9be01edaf
Samip Lamsal <lamsalsamip@outlook.com>
2023-05-22 18:48:22 +0545

    New

--- src/components/flowchart.jsx
+++ src/components/flowchart.jsx
@@ -2,33 +2,38 @@ function Flowchart(
 ) {
 
     return (
-        <div className="flex flex-col gap-y-20 bg-white py-10 px-18 md:py-16 md:px-28 w-full ">
+        <div className="flex flex-col  gap-y-5 md:gap-y-20 bg-[#202A44] py-10 px-18 md:py-16 md:px-28 w-full " id="d1">
             <div className="flex flex-col items-center gap-y-20 md:flex-row justify-around">
                 <div className="flex flex-col rounded-full p-5 md:py-14 md:px-10 justify-center gap-y-10 md:gap-y-0 md:justify-around"
-                    style={{ "background": "#FFF5F5", "height": "20rem", "width": "20rem", "boxShadow": "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+                    style={{ "background": "white", "height": "17rem", "width": "17rem", "boxShadow": "4px 4px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
                 >
-                    <p className=" text-4xl md:text-7xl text-rednew justify-center items-center text-center font-bold">8</p>
-                    <p className="text-bluenew text-2xl md:text-3xl text-center">teams compete for </p>
-                    <h3 className="text-bluenew text-2xl md:text-3xl text-center font-bold">OSMHack 2023</h3>
+                    <p id="d2" className=" text-3xl md:text-5xl text-rednew justify-center items-center text-center font-bold">8</p>
+                    <p className="text-bluenew text-xl md:text-2xl text-center">teams compete for </p>
+                    <h3 className="text-bluenew text-xl md:text-2xl text-center font-bold">OSMHack 2023</h3>
                 </div>
+
                 <div className="flex flex-col rounded-full py-14 px-10 gap-y-4 justify-between"
-                    style={{ "background": "#FFF5F5", "height": "20rem", "width": "20rem", "boxShadow": "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+                    style={{ "background": "#FFF5F5", "height": "18rem", "width": "18rem", "boxShadow": "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
                 >
-                    <p className="text-3xl md:text-5xl text-rednew justify-center items-center text-center font-bold">TOP</p>
-                    <p className="text-bluenew text-2xl md:text-3xl text-center">teams compete in nationwide </p>
-                    <h3 className="text-bluenew text-xl md:text-3xl text-center font-bold">OSM-Hackfest 2023</h3>
+                    <p className="text-2xl md:text-3xl text-rednew justify-center items-center text-center font-bold">TOP</p>
+                    <p className="text-bluenew text-xl md:text-2xl text-center">teams compete in <b className="text-[#000000]">nationwide</b> </p>
+                    <h3 className="text-bluenew text-xl md:text-2xl text-center font-bold">OSM-Hackfest 2023</h3>
                 </div>
             </div>
 
 
             <div className="flex flex-row justify-center">
-                <div className="flex flex-col rounded-full py-14 px-10 gap-y-4 justify-between"
-                    style={{ "background": "#FFF5F5", "height": "25rem", "width": "25rem", "boxShadow": "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+
+                <div className="flex flex-col rounded-full py-14 px-10 gap-y-2 justify-between items-center"
+                    style={{ "background": "#FFF5F5", "height": "20rem", "width": "20rem", "boxShadow": "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)" }}
                 >
-                    <p className="text-3xl md:text-5xl text-rednew justify-center items-center text-center font-bold">WINNER</p>
+                    <p className="-mt-[4rem] p-2 rounded-xl text-center text-lg font-bold font-inter text-[#FFA500] bg-[#202A44] w-40" style={
+                        { "filter": "drop-shadow(3px 4px 4px rgba(0, 0, 0, 0.25))" }
+                    }>FULLY SPONSORED</p>
+                    <p className="text-2xl md:text-3xl text-rednew justify-center items-center text-center font-bold">WINNER</p>
                     <p className="text-bluenew text-2xl md:text-3xl text-center">team competes in  </p>
-                    <p className="text-black text-xl md:text-3xl text-center font-bold">ASIA PACIFIC REGION</p>
-                    <h3 className="text-bluenew text-xl md:text-3xl p-5 text-center font-bold"> Regional OSM-Hackfest 2023</h3>
+                    <p className="text-black text-xl md:text-3xl text-center font-bold">ASIA PACIFIC </p>
+                    <h3 className="text-bluenew text-xl md:text-2xl p-5 text-center font-bold"> Regional OSM-Hackfest 2023</h3>
                 </div>
             </div>
 
--- src/components/prizepool.jsx
+++ src/components/prizepool.jsx
@@ -1,41 +1,58 @@
 import { BsFillTrophyFill } from 'react-icons/bs'
+import trophy1 from '../images/trophy1.png'
+import trophy2 from '../images/trophy2.png'
+import trophy3 from '../images/trophy3.png'
 function PrizePool() {
     return (
         <div className="h-30 flex flex-col p-20 justify-around items-center gap-y-20"
             style={
-                { backgroundColor: "#E7F5FF" }
+                { backgroundColor: "#2A2F4F" }
             }
         >
-            <h1 className=" text-3xl md:text-6xl text-center font-bold text-rednew">PRIZE POOL</h1>
+            <h1 className=" text-3xl md:text-5xl text-center font-bold text-white font-inter tracking-wider">PRIZE POOL</h1>
 
-            <div className='flex flex-col justify-center items-center text-black gap-y-5'>
+            {/* <div className='flex flex-col justify-center items-center text-black gap-y-5'>
                 <div>
                     <BsFillTrophyFill className='text-6xl md:text-9xl text-gold '></BsFillTrophyFill>
                 </div>
 
                 <h2 className='text-2xl md:text-3xl'>FIRST</h2>
                 <h1 className='text-2xl md:text-4xl font-bold'>Rs 25,000</h1>
-            </div>
+            </div> */}
 
             <div className='flex flex-col gap-y-16 md:flex-row justify-around w-full '>
 
                 <div className='flex flex-col justify-center items-center text-black gap-y-5'>
                     <div>
-                        <BsFillTrophyFill className='text-6xl md:text-9xl text-silver'
-                        ></BsFillTrophyFill>
+                        <img src={trophy1}></img>
                     </div>
 
-                    <h2 className='text-2xl md:text-3xl'>SECOND</h2>
-                    <h1 className='text-2xl md:text-4xl font-bold'>Rs 15,000</h1>
+
+                    <h1 className='text-2xl md:text-5xl font-bold' style={
+                        { WebkitTextStroke: '1px white' }
+                    }>Rs 25,000</h1>
                 </div>
-                <div className='flex  flex-col justify-center items-center text-black gap-y-5'>
+                <div className='flex flex-col justify-center items-center text-black gap-y-5'>
                     <div>
-                        <BsFillTrophyFill className='text-6xl md:text-9xl text-bronze '></BsFillTrophyFill>
+                        <img src={trophy2}></img>
                     </div>
 
-                    <h2 className='text-2xl md:text-3xl'>THIRD</h2>
-                    <h1 className='text-2xl md:text-4xl font-bold'>Rs 10,000</h1>
+
+                    <h1 className='text-2xl md:text-5xl font-bold' style={
+                        { WebkitTextStroke: '1px white' }
+                    }>Rs 25,000</h1>
                 </div>
+                <div className='flex flex-col justify-center items-center text-black gap-y-5'>
+                    <div>
+                        <img src={trophy3}></img>
+                    </div>
+
+
+                    <h1 className='text-2xl md:text-5xl font-bold' style={
+                        { WebkitTextStroke: '1px white' }
+                    }>Rs 25,000</h1>
+                </div>
+
             </div>
         </div>
     )
